657079964184ae90d6cab5e50ee56dc87c808f50
DevAlysonH <dev.alysonh@gmail.com>
2024-03-11 17:34:02 -0300

    Initial commit, setted layouts, and dependencies

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<AppHeader title="Home"/>
+	<div id="doc" class="col-12 p-1 d-flex rounded bg-body-secondary align-items-center justify-content-center">
+		<div>
+			<h2>Bem vindo ao gerenciador de clientes!</h2>
+			<h6>
+				Este é um simples aplicativo que permite gerenciar clientes. <br>
+				Nele, é possível salvar, editar, visualizar e excluir os clientes do seu estabelecimento. <br>
+				Para começar a gerenciar seus clientes, acesse a aba "Clientes", no menu de navegação.
+			</h6>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	import AppHeader from '../Components/AppHeader.vue';
+</script>
+
+<style scoped>
+</style>
---
+++ resources/js/Pages/Layouts/AppLayout.vue
@@ -0,0 +1,47 @@
+<template>
+	<main class="container">
+		<header>
+			<nav class="navbar navbar-expand-lg bg-body-tertiary rounded">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#">
+						<i class="fa-solid fa-people-roof"></i>
+					</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+						aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link" aria-current="page" href="#">Clientes</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</header>
+		<article class="row p-5">
+			<slot />
+		</article>
+	</main>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.navbar {
+	background-color: var(--nav-background-color) !important;
+}
+
+a {
+	color: antiquewhite;
+}
+
+a:hover,
+a:focus {
+	color: var(--nav-items-color); /* Altere para a cor desejada */
+}
+
+</style>
