7679c462488e1449d311a047ce6dfc58966e6335
DevAlysonH <dev.alysonh@gmail.com>
2024-03-11 18:57:18 -0300

    Created client index component setup

--- resources/js/Pages/Client/Index.vue
+++ resources/js/Pages/Client/Index.vue
@@ -1,11 +1,37 @@
 <template>
-<h1>Listar Clientes</h1>
+	<AppHeader title="Clientes" />
+	<h2>Clientes</h2>
+	<div class="col-12 bg-body-secondary h-auto p-2 d-flex rounded align-items-center justify-content-center">
+		<div class="card-body shadow bg-white d-flex p-1 align-items-center rounded justify-content-between">
+			<div class="ms-3">
+				<i class="fa-solid fa-user-plus"></i>
+			</div>
+			<div>
+				Client Name
+			</div>
+			<div id="actions" class="d-flex">
+				<Link href="#">
+					<button class="btn btn-primary me-2 rounded-circle">
+						<i class="fa-solid fa-eye"></i>
+					</button>
+				</Link>
+				<Link href="#">
+					<button class="btn btn-success me-2 rounded-circle">
+						<i class="fa-solid fa-pen-to-square"></i>
+					</button>
+				</Link>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-
+	import AppHeader from '../../Components/AppHeader.vue';
+	import { Link } from '@inertiajs/vue3';
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+a {
+	color: #ffff !important;
+}
 </style>
