2b81616201a8fbcf0cde92c77a54bff4a44e9ff7
DevAlysonH <dev.alysonh@gmail.com>
2024-03-12 12:04:41 -0300

    Moove navbar to a component

--- resources/js/Pages/Layouts/AppLayout.vue
+++ resources/js/Pages/Layouts/AppLayout.vue
@@ -1,24 +1,7 @@
 <template>
 	<main>
 		<header>
-			<nav class="navbar navbar-expand-lg bg-body-tertiary">
-				<div class="container-fluid">
-					<Link class="navbar-brand" href="/">
-						<i class="fa-solid fa-people-roof"></i>
-					</Link>
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-						aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse navbar-collapse" id="navbarNav">
-						<ul class="navbar-nav">
-							<li class="nav-item">
-								<Link class="nav-link" aria-current="page" href="/clients">Clientes</Link>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
+			<NavBar />
 		</header>
 		<article class="p-5">
 			<div class="container">
@@ -29,21 +12,5 @@
 </template>
 
 <script setup>
-	import { Link } from '@inertiajs/vue3';
+	import NavBar from '../../Components/NavBar.vue';
 </script>
-
-<style scoped>
-.navbar {
-	background-color: var(--nav-background-color) !important;
-}
-
-a {
-	color: antiquewhite;
-}
-
-a:hover,
-a:focus {
-	color: var(--nav-items-color); /* Altere para a cor desejada */
-}
-
-</style>
