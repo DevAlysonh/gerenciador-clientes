37a6dcf2ed0d72acccc35195a61c7f72819d5f20
DevAlysonH <dev.alysonh@gmail.com>
2024-03-13 19:53:54 -0300

    shared no content flash message

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,6 +39,7 @@ class HandleInertiaRequests extends Middleware
             'flash' => [
 				'success' => fn () => $request->session()->get('success'),
 				'fail' => fn () => $request->session()->get('fail'),
+				'no_content' => $request->session()->get('no_content'),
 			],
         ]);
     }
